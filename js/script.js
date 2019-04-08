40023da25e48c788deceb2a186a2c8f21fd98faa
a <peishanyang7@gmail.com>
2019-04-08 17:15:33 +0800

    init

---
+++ js/script.js
@@ -0,0 +1,30 @@
+import'../css/style.css';
+import'../css/normalize.css';
+import'../css/fonts/PT-Sans-Bold.eot';
+import'../css/fonts/PT-Sans-Bold.svg';
+import'../css/fonts/PT-Sans-Bold.ttf';
+import'../css/fonts/PT-Sans-Bold.woff';
+import'../css/fonts/PT-Sans-BoldItalic.eot';
+import'../css/fonts/PT-Sans-BoldItalic.svg';
+import'../css/fonts/PT-Sans-BoldItalic.ttf';
+import'../css/fonts/PT-Sans-BoldItalic.woff';
+import'../css/fonts/PT-Sans-Italic.eot';
+import'../css/fonts/PT-Sans-Italic.svg';
+import'../css/fonts/PT-Sans-Italic.ttf';
+import'../css/fonts/PT-Sans-Italic.woff';
+import'../css/fonts/PT-Sans.eot';
+import'../css/fonts/PT-Sans.svg';
+import'../css/fonts/PT-Sans.ttf';
+import'../css/fonts/PT-Sans.woff';
+
+$(document).ready(function () {
+	// when a hanburger menu is clicked
+	let $ul = $(".nav__list"),
+		 $menu = $(".nav__menu");
+
+	$($menu).click(function () {
+		// toggle menu-click Class
+		$ul.toggleClass("menu-click");
+	}); // end click event handler
+});
+
